d9c35d9ebc28701ee1b3da8d1feb87fcd48e2f65
Rhys Evans <wheresrhys@gmail.com>
2020-09-14 17:24:53 +0100

    time emoji

--- labels.js
+++ labels.js
@@ -57,31 +57,31 @@ module.exports = [
 	aliases: []
 },
 {
-	name: "S - hours",
+	name: "🕑 S - hours",
 	description: "Small effort - less than a day",
 	color: colors.claretWhite20,
 	aliases: ['good first issue']
 },
 {
-	name: "M - days",
+	name: "🕑 M - days",
 	description: "Medium effort - a day or two... or three",
 	color: colors.claretWhite40,
 	aliases: []
 },
 {
-	name: "L - weeks",
+	name: "🕑 L - weeks",
 	description: "Large effort - a week or two",
 	color: colors.claretWhite60,
 	aliases: ['L- weeks']
 },
 {
-	name: "XL - month or two",
+	name: "🕑 XL - month or two",
 	description: "Extra large effort - starting to count in months",
 	color: colors.claretWhite80,
 	aliases: []
 },
 {
-	name: "XXL - many months",
+	name: "🕑 XXL - many months",
 	description: "Extra extra large effort - this is a behemoth",
 	color: colors.claret,
 	aliases: ['XXl - many months']
